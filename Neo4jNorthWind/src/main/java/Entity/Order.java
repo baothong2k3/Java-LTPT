60d6494d9289ed5ebb44d33238428b0180432c37
H82M19\Student <Student>
2024-03-05 14:51:36 +0700

    Neo4j

---
+++ Neo4jNorthWind/src/main/java/Entity/Order.java
@@ -0,0 +1,171 @@
+package Entity;
+
+public class Order {
+    // orderID,customer,employeeID,orderDate,requiredDate,shippedDate,shipVia,freight,shipName,shipAddress,shipCity,shipRegion,shipPostalCode,shipCountry
+    private int orderID;
+    private Customer customer;
+    private int employeeID;
+    private String orderDate;
+    private String requiredDate;
+    private String shippedDate;
+    private int shipVia;
+    private double freight;
+    private String shipName;
+    private String shipAddress;
+    private String shipCity;
+    private String shipRegion;
+    private String shipPostalCode;
+    private String shipCountry;
+
+    public Order() {
+    }
+
+    public Order(int orderID, Customer customer, int employeeID, String orderDate, String requiredDate, String shippedDate, int shipVia, double freight, String shipName, String shipAddress, String shipCity, String shipRegion, String shipPostalCode, String shipCountry) {
+        this.orderID = orderID;
+        this.customer = customer;
+        this.employeeID = employeeID;
+        this.orderDate = orderDate;
+        this.requiredDate = requiredDate;
+        this.shippedDate = shippedDate;
+        this.shipVia = shipVia;
+        this.freight = freight;
+        this.shipName = shipName;
+        this.shipAddress = shipAddress;
+        this.shipCity = shipCity;
+        this.shipRegion = shipRegion;
+        this.shipPostalCode = shipPostalCode;
+        this.shipCountry = shipCountry;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getRequiredDate() {
+        return requiredDate;
+    }
+
+    public void setRequiredDate(String requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    public String getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(String shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public int getShipVia() {
+        return shipVia;
+    }
+
+    public void setShipVia(int shipVia) {
+        this.shipVia = shipVia;
+    }
+
+    public double getFreight() {
+        return freight;
+    }
+
+    public void setFreight(double freight) {
+        this.freight = freight;
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
+    }
+
+    public String getShipAddress() {
+        return shipAddress;
+    }
+
+    public void setShipAddress(String shipAddress) {
+        this.shipAddress = shipAddress;
+    }
+
+    public String getShipCity() {
+        return shipCity;
+    }
+
+    public void setShipCity(String shipCity) {
+        this.shipCity = shipCity;
+    }
+
+    public String getShipRegion() {
+        return shipRegion;
+    }
+
+    public void setShipRegion(String shipRegion) {
+        this.shipRegion = shipRegion;
+    }
+
+    public String getShipPostalCode() {
+        return shipPostalCode;
+    }
+
+    public void setShipPostalCode(String shipPostalCode) {
+        this.shipPostalCode = shipPostalCode;
+    }
+
+    public String getShipCountry() {
+        return shipCountry;
+    }
+
+    public void setShipCountry(String shipCountry) {
+        this.shipCountry = shipCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderID=" + orderID +
+                ", customer=" + customer +
+                ", employeeID=" + employeeID +
+                ", orderDate='" + orderDate + '\'' +
+                ", requiredDate='" + requiredDate + '\'' +
+                ", shippedDate='" + shippedDate + '\'' +
+                ", shipVia=" + shipVia +
+                ", freight=" + freight +
+                ", shipName='" + shipName + '\'' +
+                ", shipAddress='" + shipAddress + '\'' +
+                ", shipCity='" + shipCity + '\'' +
+                ", shipRegion='" + shipRegion + '\'' +
+                ", shipPostalCode='" + shipPostalCode + '\'' +
+                ", shipCountry='" + shipCountry + '\'' +
+                '}';
+    }
+}
